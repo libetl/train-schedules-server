0e808c639cc706cd6873e00f280b988edf59e62b
libetl <libe4@free.fr>
2018-02-01 00:06:57 +0100

    removing the dump feature, because it does not work on heroku

--- trainSchedulesServer.js
+++ trainSchedulesServer.js
@@ -2,19 +2,19 @@ const moment = require('moment')
 const express = require('express')
 const haversine = require('./haversine')
 const {nearestTo, timetable, asDeparturesData} = require('./timetable')
-const {update, updateWithoutWrite, wakeUpDumpIfNecessary} = require('./dumpReadAndWrite')
+const {updateWithoutWrite} = require('./dumpReadAndWrite')
 
 const gtfs = {}
 const app = express()
 
-const workWith = ({res, gtfs, coords, date, time}) => wakeUpDumpIfNecessary(gtfs).then(() => {
+const workWith = ({res, gtfs, coords, date, time}) => {
     const coordinates = {lat: parseFloat(coords.split(',')[0]), long: parseFloat(coords.split(',')[1])}
     const stopPoints = nearestTo(coordinates, gtfs)
     const distanceKilometers = stopPoints[0] && haversine(coordinates,
         {lat: parseFloat(stopPoints[0].stop_lat), long: parseFloat(stopPoints[0].stop_lon)})
     res.set('Content-Type', 'application/json')
     res.send({departures: asDeparturesData(timetable({gtfs, stopPoints,date, time})),
-        stationName: (stopPoints[0] || {}).stop_name || 'no station nearby', date, time, distanceKilometers})})
+        stationName: (stopPoints[0] || {}).stop_name || 'no station nearby', date, time, distanceKilometers})}
 
 app.get('/coords/:coords', ({params:{coords}}, res) => workWith(
     {res, gtfs, coords, date: moment().format('YYYYMMDD'), time: moment().format('HH:mm:ss')}))
@@ -46,7 +46,6 @@ app.get('/', (options, res) => res.set('Content-Type', 'application/json') &&
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), () => console.log(`Train schedule server on port ${app.get('port')}`))
 
-update(gtfs).then(() => console.log('I am all set') &&
-    setInterval(() =>
-        get('https://train-schedules-server.herokuapp.com').then(() => update(gtfs)), 1000000))
+updateWithoutWrite(gtfs).then(updatedGtfs =>
+    Object.assign(gtfs, updatedGtfs, {freshness: moment().format('YYYY-MM-DDTHH:mm:ss')}))
 
