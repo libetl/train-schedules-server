d408cd55a3edd17b1110be0b798377083f9c392d
lbenychou <lbenychou@egencia.com>
2018-04-11 12:07:46 +0200

    excluding transilien for now

--- sources.js
+++ sources.js
@@ -1,5 +1,5 @@
 module.exports = [
     'https://ressources.data.sncf.com/explore/dataset/sncf-ter-gtfs/files/24e02fa969496e2caa5863a365c66ec2/download/',
     'https://ressources.data.sncf.com/explore/dataset/sncf-intercites-gtfs/files/ed829c967a0da1252f02baaf684db32c/download/',
-    'https://ressources.data.sncf.com/explore/dataset/sncf-transilien-gtfs/files/f1a8e58126fbe2f67dd0512ef74a1124/download/',
-    'https://ressources.data.sncf.com/explore/dataset/sncf-transilien-gtfs/files/023d3733775238ae2e431e3613812bae/download/']
+    /*'https://ressources.data.sncf.com/explore/dataset/sncf-transilien-gtfs/files/f1a8e58126fbe2f67dd0512ef74a1124/download/',
+    'https://ressources.data.sncf.com/explore/dataset/sncf-transilien-gtfs/files/023d3733775238ae2e431e3613812bae/download/'*/]
